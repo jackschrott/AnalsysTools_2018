161a1db92482aa809f8219a6255f1fa67c4b4e58
jackschrott <jack.schrott@gmail.com>
2018-06-21 15:18:25 -0500

    Update RUNmt2tree.C

--- mt2Looper/RUNmt2tree.C
+++ mt2Looper/RUNmt2tree.C
@@ -2,6 +2,9 @@
 void RUNmt2tree(){
   TChain *chain = new TChain("mt2","");
   chain->Add("/nfs-6/userdata/mt2/V00-08-18_WHstudy/TChiWH_700_1_part1.root");
+  chain->Add("/nfs-6/userdata/mt2/V00-08-18_WHstudy/TChiWH_700_1_part2.root");
+  chain->Add("/nfs-6/userdata/mt2/V00-08-18_WHstudy/TChiWH_700_1_part3.root");
+  chain->Add("/nfs-6/userdata/mt2/V00-08-18_WHstudy/TChiWH_700_1_part4.root");
   mt2tree a(chain);
   a.Loop();
 }
